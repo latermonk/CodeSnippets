608a9a694f5e421e84673b6f80f69c0b2b215913
latermonk <sprout_wang@163.com>
2017-10-17 14:12:00 +0800

    co

---
+++ iOS/02_UI/UIViewController/UIViewController/MYViewController.h
@@ -0,0 +1,13 @@
+//
+//  MYViewController.h
+//  UIViewController
+//
+//  Created by wei on 17/10/2017.
+//  Copyright © 2017 wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYViewController : UIViewController
+
+@end
---
+++ iOS/02_UI/UIViewController/UIViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewController
+//
+//  Created by wei on 17/10/2017.
+//  Copyright © 2017 wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
